ba637bce4fb8c7931ec2e538acaf71fde6ca041d
zhenghang <1170723706@qq.com>
2019-01-18 16:41:45 +0800

    Initial commit

---
+++ twodome/src/main/java/com/jk/controller/sdad.java
@@ -0,0 +1,17 @@
+package com.jk.controller;
+
+/**
+ * @program: first
+ * @description:
+ * @author: Mr.Xue
+ * @create: 2019-01-18 16:29
+ **/
+public class sdad {
+
+    public String test01(){
+        String asd = "2364564";
+        System.out.println(asd);
+        return "asd";
+    }
+
+}
